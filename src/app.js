1ad3e3e00c8c2d6d1b73227bbe8ebb532e255828
Kyle Pfromer <kpfromer2@gmail.com>
2018-08-28 20:23:32 -0600

    Add file uploads and email forwarding

--- src/app.js
+++ src/app.js
@@ -1,61 +1,63 @@
 import MailListener from 'mail-listener2-updated';
-import { sendMessage } from './slack';
+import { sendMessage, sendFile, sendEmail, test } from './slack';
+import fs from 'fs';
 
+const deleteFile = path => valueFromPromise =>
+  new Promise((resolve, reject) => 
+    fs.unlink(path, (error) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve(valueFromPromise);
+    })
+  );
 
 const mailListener = new MailListener({
   username: process.env.EMAIL_LOGIN,
-  password: process.env.EMAIL_PASS, // We have to make sure that the server is secure!
-  host: 'imap.gmail.com', // THIS WILL BE GMAIL
-  port: 993, // imap port // MAKE THIS GMAIL
-  tls: true,
-  connTimeout: 10000, // Default by node-imap
-  authTimeout: 5000, // Default by node-imap,
-  debug: console.log, // Or your custom function with only one incoming argument. Default: null
+  password: process.env.EMAIL_PASS,
+  host: 'imap.gmail.com',
+  port: 993, // imap port
+  tls: true, // Required for secure connection google
+  connTimeout: 10000,
+  authTimeout: 5000,
+  debug: console.log,
   tlsOptions: { rejectUnauthorized: false },
   mailbox: "INBOX", // mailbox to monitor
   // searchFilter: ["UNSEEN", "FLAGGED"], // the search filter being used after an IDLE notification has been retrieved
-  markSeen: true, // all fetched email willbe marked as seen and not fetched next time
+  markSeen: false, // all fetched email willbe marked as seen and not fetched next time
   fetchUnreadOnStart: true, // use it only if you want to get all unread email on lib start. Default is `false`,
-  mailParserOptions: {streamAttachments: true}, // options to be passed to mailParser lib.
+  // mailParserOptions: {streamAttachments: true}, // options to be passed to mailParser lib.
   attachments: true, // download attachments as they are encountered to the project directory
-  attachmentOptions: { directory: "attachments/" } // specify a download directory for attachments
+  // attachmentOptions: { directory: "./attachments/" } // specify a download directory for attachments
 });
 
+mailListener.start();
 
-mailListener.start(); // start listening
-
-// stop listening
-//mailListener.stop();
-
-mailListener.on("server:connected", function(){
+mailListener.on("server:connected", () => {
   sendMessage('connected');
-  console.log("imapConnected");
 });
 
-mailListener.on("server:disconnected", function(){
+mailListener.on("server:disconnected", () => {
   sendMessage('disconnected');
-  console.log("imapDisconnected");
 });
 
-mailListener.on("error", function(err){
-  sendMessage('error');
-  console.log(err);
+mailListener.on("error", error => 
+  sendMessage(`Error encountered: \n ${error}`)
+);
+
+mailListener.on("mail", mail => {
+  sendEmail(mail.subject, mail.from[0].name, mail.text)
+    .catch(error =>
+      sendMessage(`Something went wrong sending an email: ${error}`)
+    );
 });
 
-mailListener.on("mail", function (mail, seqno, attributes) { // SEE https://nodemailer.com/extras/mailparser/
-  // do something with mail object including attachments
-  console.log("emailParsed", mail);
-
-  // POST TO SLACK!
-  // See: https://api.slack.com/methods/chat.postMessage
-  sendMessage(mail.text);
-
-  // mail processing code goes here
+mailListener.on("attachment", attachment => {
+  const { fileName, path } = attachment;
+  const fileTypeArray = fileName.split('.');
+  sendFile(fileName, fileTypeArray[fileTypeArray.length - 1], path)
+    .then(deleteFile(path))
+    .catch(error => 
+      sendMessage(`Something went wrong uploading a file: ${error}`)
+    );
 });
-
-mailListener.on("attachment", function(attachment){
-  console.log(attachment.path);
-});
-
-// it's possible to access imap object from node-imap library for performing additional actions. E.x.
-// mailListener.imap.move(:msguids, :mailboxes, function(){})
--- src/slack.js
+++ src/slack.js
@@ -1,10 +1,45 @@
 import { WebClient } from '@slack/client';
+import SlackUpload from 'node-slack-upload';
+import fs from 'fs';
 const token = process.env.SLACK_TOKEN;
 const emailChannelId = process.env.EMAIL_CHANNEL_ID;
-const web = new WebClient(token);
+
+const web = new WebClient(token); // Basic slack client
+const slack = new SlackUpload(token); // Better slack client for uploading things
+
 export function sendMessage(text) {
   return web.chat.postMessage({
     channel: emailChannelId,
     text
-  })
+  });
+}
+
+const asyncUpload = (options) => {
+  return new Promise((resolve, reject) =>
+    slack.uploadFile(options, (error, data) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve(data);
+    })
+  )
+}
+
+export function sendEmail(subject, from, text) {
+  return asyncUpload({
+    content: text,
+    filetype: 'txt',
+    title: `Email`,
+    initialComment: `Email from "${from}" about "${subject}"`,
+    channels: emailChannelId
+  });
 };
+
+export function sendFile(fileName, filetype, filePath) {
+  return asyncUpload({
+    file: fs.createReadStream(filePath),
+    filetype,
+    title: fileName,
+    channels: emailChannelId
+  });
+}
