55b6ed3312c3c6966d821d85320ea7c9210c887e
Kyle Pfromer <kpfromer2@gmail.com>
2018-08-28 21:18:28 -0600

    Update webpack to bundle env vars with build

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,14 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
+  mode: 'production',
   entry: './src/app.js',
   target: 'node',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'backend.js'
   },
-  mode: 'production'
+  plugins: [
+    new webpack.EnvironmentPlugin(['EMAIL_LOGIN', 'EMAIL_PASS', 'SLACK_TOKEN', 'EMAIL_CHANNEL_ID'])
+  ]
 };
